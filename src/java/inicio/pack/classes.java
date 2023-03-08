2a211b7485768c49f83069836f1d7352bedd561c
Samir Guimarães <samir0312@outlook.com.br>
2023-03-07 21:27:29 -0300

    "Não pushado ainda"

--- src/java/inicio/pack/classes.java
+++ src/java/inicio/pack/classes.java
@@ -3,3 +3,4 @@ package java.inicio.pack;
 public class classes {
 
 }
+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
---
+++ src/java/inicio/pack/token_github.java
@@ -0,0 +1,5 @@
+package java.inicio.pack;
+
+public class token_github {
+// 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
+}
